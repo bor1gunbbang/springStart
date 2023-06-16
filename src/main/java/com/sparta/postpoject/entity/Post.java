65afaddcddfad571f08a9c93f5d4a97c97728a63
bor1gunbbang <whl564@naver.com>
2023-06-16 09:40:22 +0900

    auditing 수정

--- src/main/java/com/sparta/postpoject/entity/Post.java
+++ src/main/java/com/sparta/postpoject/entity/Post.java
@@ -1,21 +1,26 @@
 package com.sparta.postpoject.entity;
 
 import com.sparta.postpoject.dto.PostRequestDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
 
-import java.security.PrivateKey;
-import java.util.PrimitiveIterator;
-
+import java.time.LocalDateTime;
+@Entity
 @Getter
 @Setter
+@Table(name = "Post")
 @NoArgsConstructor
 public class Post extends Timestamped{
+    @Id
     private Long id;
     private String username;
     private String contents;
-    private double password;
+    private Double password;
     private String title;
 
     public Post(PostRequestDto requestDto) {
@@ -31,5 +36,13 @@ public class Post extends Timestamped{
         this.title = requestDto.getTitle();
         this.contents = requestDto.getContents();
 
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
